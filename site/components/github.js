12ef1071a91d443d87bcdb2c00ec75944e81e23f
hjoelh <jhollingsmith@gmail.com>
2021-02-16 11:50:09 +0000

    improve lighthouse

--- site/components/github.js
+++ site/components/github.js
@@ -3,7 +3,12 @@ import { IconContext } from "react-icons";
 
 const Github = () => {
   return (
-    <a className='github' href="https://github.com/hjoelh/Lofi-Radio-Desktop" target="_blank">
+    <a
+      rel="noopener"
+      className="github"
+      href="https://github.com/hjoelh/Lofi-Radio-Desktop"
+      target="_blank"
+    >
       <IconContext.Provider value={{ color: "white", size: "2.5em" }}>
         <VscGithub />
       </IconContext.Provider>
--- site/components/producthunt.js
+++ site/components/producthunt.js
@@ -1,6 +1,7 @@
 export default function Producthunt() {
   return (
     <a
+      rel="noopener"
       className="productHunt"
       href="https://www.producthunt.com/posts/lofi-radio-desktop?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-lofi-radio-desktop"
       target="_blank"
