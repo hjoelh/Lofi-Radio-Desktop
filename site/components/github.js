33ccf81050144b5daf21bdd81d5dbb901bb630da
Joel <68335961+hjoelh@users.noreply.github.com>
2021-06-19 17:24:08 +0100

    fix github link

--- site/components/github.js
+++ site/components/github.js
@@ -5,7 +5,7 @@ export default function Github() {
     <a
       rel="noreferrer"
       className="github"
-      href="https://github.com/hjoelh/Eth-Price-Bot"
+      href="https://github.com/hjoelh/Lofi-Radio-Desktop"
       target="_blank"
       aria-label="GitHub"
       style={{
